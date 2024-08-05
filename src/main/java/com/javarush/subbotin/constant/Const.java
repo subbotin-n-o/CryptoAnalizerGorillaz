b819e18319ec3fb88a66f2b496677af33afe174b
nikitasubbotin <subbotin.n.o@gmail.com>
2024-08-05 21:48:00 +0400

    add Const.java

---
+++ src/main/java/com/javarush/subbotin/constant/Const.java
@@ -0,0 +1,13 @@
+package com.javarush.subbotin.constant;
+
+import java.io.File;
+
+public interface Const {
+    String ENCODE = "encode";
+    String DECODE = "decode";
+
+    String TXT_FOLDER = System.getProperty("user.dir") +
+            File.separator +
+            "text" +
+            File.separator;
+}
